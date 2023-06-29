9ef6fd1117147864bc25936500472921b8bf6aad
Cruuise <ed.cruz198@gmail.com>
2023-06-28 21:03:31 -0700

    Added script to auto update the month.year

---
+++ assets/js/extraneous.js
@@ -0,0 +1,4 @@
+var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Ocotber', 'Novemner', 'December'];
+var date = new Date();
+
+document.getElementById('test').innerHTML = month[date.getMonth()] + ' ' + date.getUTCFullYear();
