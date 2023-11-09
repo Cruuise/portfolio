49cbe2f2fd2a43292a00cefe34f89f2b74e3f419
Edgar Cruz <ed.cruz198@gmail.com>
2023-11-08 19:16:59 -0800

    Fixed "November" typo

--- assets/js/extraneous.js
+++ assets/js/extraneous.js
@@ -1,4 +1,4 @@
-var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Ocotber', 'Novemner', 'December'];
+var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Ocotber', 'November', 'December'];
 var date = new Date();
 
 document.getElementById('test').innerHTML = month[date.getMonth()] + ' ' + date.getUTCFullYear();
